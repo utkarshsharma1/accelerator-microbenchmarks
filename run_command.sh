55344c205d094a2719a2998ad7195c48f2f2ecdc
Utkarsh Sharma <utksharma@google.com>
2025-08-22 10:46:12 +0000

    Testing all reduce

--- run_command.sh
+++ run_command.sh
@@ -4,12 +4,12 @@ export ZONE="europe-west4-b"
 export PROJECT_ID="cloud-tpu-multipod-dev"
 # TODO change it to the TPU type you want to run the workload on.
 # For example, v5p-128 or v5p-256.
-export TPU_TYPE="v5p-256"
+export TPU_TYPE="v5p-128"
 export NUM_SLICES=1
 
 # TODO change it
 # This is the name of the workload that will be created in the xpk command.
-export WORKLOAD_NAME="utksharma-tpu-mb-v5p-256"
+export WORKLOAD_NAME="utksharma-tpu-mb-v5p-128"
 
 gcloud config set project ${PROJECT_ID}
 gcloud config set compute/zone ${ZONE}
@@ -23,8 +23,8 @@ xpk workload create \
   --device-type=${TPU_TYPE} \
   --command="git clone https://github.com/utkarshsharma1/accelerator-microbenchmarks.git \
   && cd accelerator-microbenchmarks && pip install -r requirements.txt && \
-  python src/run_benchmark.py --config=configs/xlml_v5p_256_utksharma.yaml \
-  && gsutil -m cp -r /tmp/microbenchmarks gs://v5p-microbenchmarks/v5p-256-$(date +%Y-%m-%d-%H-%M-%S)/" \
+  python src/run_benchmark.py --config=configs/xlml_v5p_128_utksharma.yaml \
+  && gsutil -m cp -r /tmp/microbenchmarks gs://v5p-microbenchmarks/v5p-128-$(date +%Y-%m-%d-%H-%M-%S)/" \
   --num-slices=${NUM_SLICES} \
   --docker-image=us-docker.pkg.dev/cloud-tpu-images/jax-stable-stack/tpu:jax0.5.2-rev1 \
   --workload=${WORKLOAD_NAME}
