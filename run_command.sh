25e2711cf876292f8ded3e5505efa1eb498b7ce2
Prisha Jain <prishajain@google.com>
2025-08-25 20:49:32 +0000

    Changed to env vars

--- run_command.sh
+++ run_command.sh
@@ -1,11 +1,14 @@
-CLUSTER_NAME="mlperf-v5p"
-REGION="europe-west4"
-ZONE="europe-west4-b"
-PROJECT_ID="cloud-tpu-multipod-dev"
-DOCKER_IMAGE="us-docker.pkg.dev/cloud-tpu-images/jax-stable-stack/tpu:jax0.5.2-rev1"
-RUN_ID=$(date +%Y-%m-%d-%H-%M-%S)
-TPU_TYPE=v5p-128
-GCS_BASE_PATH="gs://v5p-microbenchmarks/report_data_${RUN_ID}"
+export CLUSTER_NAME=#<cluster_name>
+export REGION=#<region>
+export ZONE=#<zone>
+export PROJECT_ID=#<project_id>
+export DOCKER_IMAGE="us-docker.pkg.dev/cloud-tpu-images/jax-stable-stack/tpu:jax0.5.2-rev1"
+export RUN_ID=$(date +%Y-%m-%d-%H-%M-%S)
+
+# Change as per need (v5p-128/v5p-256)
+export TPU_TYPE=v5p-128
+export GCS_BASE_PATH="gs://v5p-microbenchmarks"
+export GCS_PATH="${GCS_BASE_PATH}/report_data_${RUN_ID}"
 
 # gcloud setup
 gcloud config set project ${PROJECT_ID}
@@ -13,10 +16,13 @@ gcloud config set compute/zone ${ZONE}
 gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${REGION} --project ${PROJECT_ID}
 
 # Benchmark variables
-CONFIG_FILE="configs/xlml_v5p_128_utksharma.yaml"
+CONFIG_FILE=#<config_file>
+
+# TODO: change workload name
 WORKLOAD_NAME="prisha-mb-${TPU_TYPE}"
-GCS_JSONL_PATH="${GCS_BASE_PATH}/${TPU_TYPE}/metrics_report.jsonl"
-GCS_EXCEL_PATH="${GCS_BASE_PATH}/${TPU_TYPE}_benchmark_report.xlsx"
+
+GCS_JSONL_PATH="${GCS_PATH}/${TPU_TYPE}/metrics_report.jsonl"
+GCS_EXCEL_PATH="${GCS_PATH}/${TPU_TYPE}_benchmark_report.xlsx"
 XPK_COMMAND="git clone https://github.com/utkarshsharma1/accelerator-microbenchmarks.git && \
 cd accelerator-microbenchmarks && \
 pip install -r requirements.txt && \
