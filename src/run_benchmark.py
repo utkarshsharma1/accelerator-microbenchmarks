d3a147298c86f4bffb7016b5b8fee20b84b47768
Hong-Yi Lin <horacehylin@google.com>
2025-08-26 07:00:24 +0000

    Change delimiter from , to \t.
    .
    
    .

--- src/run_benchmark.py
+++ src/run_benchmark.py
@@ -262,7 +262,7 @@ def write_to_csv(csv_path: str, calculate_metrics_results: List[Dict[str, Any]])
     df_list = [convert_dict_to_df(each) for each in calculate_metrics_results]
     df = pd.concat(df_list, ignore_index=True)
 
-    df.to_csv(csv_path, index=False)
+    df.to_csv(csv_path, index=False, sep="\t")
 
     print(f"Metrics written to CSV at {csv_path}.")
 
